20ef47f0bb0815741c91704b262cb35e872a5ea0
Playingbinke6 <91207302+Playingbinke6@users.noreply.github.com>
2021-12-07 09:49:18 -0600

    Update Facts.java

--- Facts.java
+++ Facts.java
@@ -3,7 +3,15 @@ import java.io.*;
 
 public class Facts 
 {
-
+  public static void main(String[] args)
+   {
+      Scanner userIn = new Scanner(System.in);
+      System.out.println("Wlecome to the randome facts genaratore!");
+      System.out.println("what facts would you like to learn about?");
+      String factCat = userIn.nextLine();
+      //need to orginise in to whitch cat they whant 
+      //and only alow the randome to pull form those
+   }
 
 
 
