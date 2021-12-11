a21724d7afdc3a5580286f38aa8827dea226689d
Playingbinke6 <91207302+Playingbinke6@users.noreply.github.com>
2021-12-10 20:15:20 -0600

    Update FactsRun.java

--- FactsRun.java
+++ FactsRun.java
@@ -9,6 +9,7 @@ public class FactsRun
       System.out.println("Wlecome to the randome facts genaratore!");
       System.out.println("we have facts about: \nanimals\nhistory\ntravel\nmusic\nsports\nhuman\nfunny\nmiscellaneous");
       Facts arrays = new Facts();
+      arrays.file();
       String[] animal = arrays.getAnimal();//pulling the arrays from the facts file and putting them in to arrays for the run methoud to use
       String[] history = arrays.getHistory();
       String[] travel = arrays.getTravel();
@@ -27,50 +28,65 @@ public class FactsRun
             System.out.println("We have " + animal.length + " facts about animals.  \nPick a number from 1-" + animal.length);//user pickes what fact to look at
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(animal[index]);//pulls fact from array of the catagory and prints to the screen from user input
+            System.out.println(animal[index+1]);//pulls fact from array of the catagory and prints to the screen from user input
+            //+1 is so that the index entered by the user matches with index of arrays
             
                
          }else if(factCat.equals("history facts")){
-            System.out.println("We have " + history.length + " facts about history.  \nPick a number from 1-" + history.length);
+            int len = history.length;
+            len--;len--;
+            System.out.println("We have " + len + " facts about history.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(history[index]);
+            System.out.println(history[index+1]);
             
          }else if(factCat.equals("travel facts")){
-            System.out.println("We have " + travel.length + " facts about traveling.  \nPick a number from 1-" + travel.length);
+            int len = travel.length;
+            len--;len--;
+            System.out.println("We have " + len + " facts about traveling.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(travel[index]);
+            System.out.println(travel[index+1]);
             
          }else if(factCat.equals("music facts")){
-            System.out.println("We have " + music.length + " facts about music.  \nPick a number from 1-" + music.length);
+            int len = music.length;
+            len--;len--;
+            System.out.println("We have " + len + " facts about music.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(music[index]);
+            System.out.println(music[index+1]);
             
          }else if(factCat.equals("sports facts")){
-            System.out.println("We have " + sports.length + " facts about sports.  \nPick a number from 1-" + sports.length);
+            int len = sports.length;
+            len--;len--;
+            System.out.println("We have " + len + " facts about sports.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(sports[index]);
+            System.out.println(sports[index+1]);
             
          }else if(factCat.equals("human facts")){
-            System.out.println("We have " + human.length + " facts about humans.  \nPick a number from 1-" + human.length);
+            int len = human.length;
+            len--;len--;
+            System.out.println("We have " + len + " facts about humans.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(human[index]);
+            System.out.println(human[index+1]);
             
          }else if(factCat.equals("funny facts")){
-            System.out.println("We have " + funny.length + " funny facts.  \nPick a number from 1-" + funny.length);
+            int len = funny.length;
+            len--;len--;
+            System.out.println("We have " + len + " funny facts.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(funny[index]);
+            System.out.println(funny[index+1]);
             
          }else if(factCat.equals("miscellaneous facts")){
-            System.out.println("We have " + misc.length + " miscellaneous facts.  \nPick a number from 1-" + misc.length);
+            int len = misc.length;
+            len--;len--;
+            System.out.println("We have " + len + " miscellaneous facts.  \nPick a number from 1-" + len);
             String fact = userIn.nextLine();
             int index = Integer.parseInt(fact);
-            System.out.println(misc[index]);
+            System.out.println(misc[index+1]);
             
          }else if(factCat.equals("stop")){//will end the program if users inputs stop, then prints our names
             System.out.println("thank you for using the Fact Genorator/nCreated by: Aaron donetaly and Teddy Clark");
